b9ea9997b2e22059c3d85f1968a72f12227b4e9f
meganeroppy <roppy@hotmail.co.jp>
2017-12-23 02:39:36 +0900

    更新

---
+++ localServerTest/Assets/CPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+public class CPlayer : MonoBehaviour {
+	private Rigidbody myRigidbody;
+	public float fSpeed = 10f;
+	// Use this for initialization
+	void Start () {
+		myRigidbody = GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 move = new Vector3(
+			Input.GetAxisRaw("Horizontal"),
+			Input.GetAxisRaw("Vertical"));
+		myRigidbody.velocity = fSpeed * move.normalized;
+	}
+}
