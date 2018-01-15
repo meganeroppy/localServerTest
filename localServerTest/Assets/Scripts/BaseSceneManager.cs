9376be40bb36e203492a82d701d4ff9ee9deb6b1
meganeroppy <roppy@hotmail.co.jp>
2018-01-15 21:25:21 +0900

    BaseSceneにプリセットカメラを追加しデフォルト無効

--- localServerTest/Assets/Scripts/BaseSceneManager.cs
+++ localServerTest/Assets/Scripts/BaseSceneManager.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class BaseSceneManager : MonoBehaviour {
+public class BaseSceneManager : MonoBehaviour
+{
+    public static BaseSceneManager instance;
 
     public string firstSceneName;
 
+    [SerializeField]
+    private Camera[] presetCameras;
+
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
+        instance = this;
         SceneManager.LoadScene(firstSceneName, LoadSceneMode.Additive);
 	}
-	
+
+    public void ActivatePresetCameras()
+    {
+        for( int i = 0 ; i < presetCameras.Length; ++i )
+        {
+            presetCameras[i].enabled = true;
+            presetCameras[i].targetDisplay = i+2;
+        }
+    }
 }
